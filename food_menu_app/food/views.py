ae804ca00a0242e0dac0c9a28d5a5ba326f3b5b0
Pankaj Kumar <pankajnee2@gmail.com>
2022-07-26 13:43:56 +0530

    using render function to display template

--- food_menu_app/food/views.py
+++ food_menu_app/food/views.py
@@ -6,9 +6,11 @@ from django.template import loader
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
+    # template = loader.get_template('food/index.html')
     context ={'item_list':item_list} #for DB contents
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request,'food/index.html',context)
+
 
 def item(request):
     return HttpResponse("<h1> This ia an item.</h1>")

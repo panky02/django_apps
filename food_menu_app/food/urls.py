36a6127abf76db0644180a6b0671d1ae6b0928f5
Pankaj Kumar <pankajnee2@gmail.com>
2022-07-27 21:21:47 +0530

    adding an initial detail view

--- food_menu_app/food/urls.py
+++ food_menu_app/food/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('item/', views.item, name='item')
+    path('<int:item_id>', views.detail, name='detail'),
+    path('item/', views.item, name='item'),
 ]
--- food_menu_app/food/views.py
+++ food_menu_app/food/views.py
@@ -14,3 +14,6 @@ def index(request):
 
 def item(request):
     return HttpResponse("<h1> This ia an item.</h1>")
+
+def detail(request,item_id):
+    return HttpResponse("this is item no/id:{}".format(item_id))

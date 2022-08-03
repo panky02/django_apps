dc20ccd52c12f91e3e80f70ceea939c88db6ce9b
Pankaj Kumar <pankajnee2@gmail.com>
2022-08-03 20:28:50 +0530

    added update n delete for foodapp

--- food_menu_app/food/urls.py
+++ food_menu_app/food/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('item/', views.item, name='item'),
     #add items via form
     path("add",views.create_item,name="create_item"),
+    path('update/<int:id>/',views.update_item, name="update_item"),
+    path('delete/<int:id>/',views.delete_item,name='delete_item'),
 ]

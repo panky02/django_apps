866baf139943d84fa2c52135466f62271eb7d970
Pankaj Kumar <pankajnee2@gmail.com>
2022-08-08 06:27:51 +0530

    Initialised Moviesdata model

--- movies_api/movies/models.py
+++ movies_api/movies/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Moviedata(models.Model):
+    name = models.CharField(max_length = 200)
+    durations = models.FloatField()
+    rating = models.FloatField()

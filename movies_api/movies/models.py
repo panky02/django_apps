47eea1d83dce60dd4cff3f42456b362b6a219cfd
Pankaj Kumar <pankajnee2@gmail.com>
2022-08-08 06:33:21 +0530

    Added model to admin site & str meth in moviemodel

--- movies_api/movies/models.py
+++ movies_api/movies/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Moviedata(models.Model):
+
+    def __str__(self) -> str:
+        return self.name
+
     name = models.CharField(max_length = 200)
     durations = models.FloatField()
     rating = models.FloatField()
